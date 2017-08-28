6e215c5752034787721f3be3ae7f372b1d520d94
saverio belviso <sbelviso@gmail.com>
2017-08-28 10:14:09 +0200

    progetto fc

---
+++ fc/fc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fc.MvcApplication" Language="C#" %>
---
+++ fc/fc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(fc.Startup))]
+namespace fc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
